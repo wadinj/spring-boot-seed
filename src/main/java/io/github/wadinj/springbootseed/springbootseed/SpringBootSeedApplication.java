13bb716b32fa83a77376e6bd8e30ba6d89c3b425
Jonathan Wadin <wadin.jonathan@gmail.com>
2019-05-06 14:34:49 +0200

    :tada: spring initializr and .gitignore

---
+++ src/main/java/io/github/wadinj/springbootseed/springbootseed/SpringBootSeedApplication.java
@@ -0,0 +1,13 @@
+package io.github.wadinj.springbootseed.springbootseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeedApplication.class, args);
+	}
+
+}
