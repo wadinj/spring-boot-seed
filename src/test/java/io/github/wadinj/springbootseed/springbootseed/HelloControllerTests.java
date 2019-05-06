6c5676205d944caa65f447dd356c1afd10e08c0f
Jonathan Wadin <wadin.jonathan@gmail.com>
2019-05-06 15:11:44 +0200

    :tada: Add travis CI build

--- src/test/java/io/github/wadinj/springbootseed/springbootseed/HelloControllerTests.java
+++ src/test/java/io/github/wadinj/springbootseed/springbootseed/HelloControllerTests.java
@@ -1,4 +1,42 @@
 package io.github.wadinj.springbootseed.springbootseed;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTests {
+
+    @LocalServerPort
+    private int port;
+
+    private URL baseUrl;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        this.baseUrl = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void whenCallingGetPost_thenReturnsAllPosts() {
+
+        ResponseEntity<String> response = template.getForEntity(baseUrl.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Hello World!"));
+    }
 }
